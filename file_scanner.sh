16f7d76a9b32e3e6ce15cc3b1375cd75c1ffebdd
shyamriversafe <113644831+shyamriversafe@users.noreply.github.com>
2022-09-29 05:19:24 -0700

    Added Comments

--- file_scanner.sh
+++ file_scanner.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-echo -n "Enter path you want to scan in the format c:/folder1/folder2: "
-DIR=/app/files/
-#Scans all files in specified folder or directory
+
+#echo -n "Enter path you want to scan in the format c:/folder1/folder2: "
+
+DIR=/app/files/ # Path is set to files folder
+
+#Lists all files in specified folder or directory and sort them out accordingly
  files=$(ls $DIR | sed 's/^.*\.//' | sort -u)
  
  #echo -n $scan_files
  
-for file in $files
+for file in $files 
  do
-	echo $file
-	mkdir $DIR/$file
-	cp $DIR/*.$file $DIR/$file/
+	echo $file						 
+	mkdir $DIR/$file				# Creates the folders based on file extensions
+	cp $DIR/*.$file $DIR/$file/		# copy specic extension files to the folder created
 done
