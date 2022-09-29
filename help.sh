e2348eb10a670938737eedaea5924de547c93d8b
shyamriversafe <113644831+shyamriversafe@users.noreply.github.com>
2022-09-29 05:19:56 -0700

    Updated the content of the file

--- help.sh
+++ help.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-echo ''
-echo '***HELP*********HELP*********HELP*********HELP*********'
-echo ''
-echo -e 'This code is used to sort the files and copy them to the folder based on the extensions. ** It creates new folder based on the file extensions'
+# ---------------------------------------------------------------
+#                               HELP                             
+# ---------------------------------------------------------------
+# This code is used to sort the files and copy them to the folder based on the extensions.
+# Options
+# option --help
+# option --version
+# option --scan
+# help option will display the instructons on how to use this application
+# version option will print the version of the docker images
+# scan option will scan the files in the foder and sort them out by creating new folder based on the file extension.
+# Once the folder is craeted all the related files are moved to that folder.
+# For eg: If you folder with file extensions .txt, .sh, .pdf, .jpg 
+# This code will first create folders with names txt, shx, pdf and jpg.
+# Once the folders are created then they are moved to the particular folder of their extensions.
+
